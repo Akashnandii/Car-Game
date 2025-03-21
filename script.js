48ef9acc8c11a21fb1dd3a2ab1504fceb15553e4
Akash <nandiakash96@gmail.com>
2025-03-21 21:21:53 +0530

    updates CSS to fix wheel positioning

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-// var audio = document.createElement('audio');
-// audio.setAttribute('src', 'sound.mp3');
-// audio.loop = true
-// audio.play();
-
-
-
 var audio = document.createElement('audio');
 audio.setAttribute('src', 'sound.mp3');
 audio.loop = true;
 
 var isPlaying = false; // Track audio state
 
-document.addEventListener('click', function() {
+document.addEventListener('click', function () {
     if (isPlaying) {
         audio.pause();  // Stop the audio
-    } else {
+    }
+    else {
         audio.play();   // Play the audio
     }
     isPlaying = !isPlaying; // Toggle state
